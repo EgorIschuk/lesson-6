41140b24e3557ef4e9b1c1ff95ee8dd3a661ee74
EgorIschuk <egorisuk21@gmail.com>
2024-07-01 19:33:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("main-button")
+        const text = document.addEventListener("click", () => 
+            {document.getElementById("main-text")
+        if (text.style.color ==="blue") {
+            text.style.color ="red";
+        } else {
+            text.style.color = "blue";
+        }
+    }
+   );
